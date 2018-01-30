ec35544000a4162697144fdb4164d897584db7af
linpenghui958 <370345882@qq.com>
2018-01-30 22:20:53 +0800

    modify webpack.config.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
-'use strict'
 var React = require('react')
 var ReactDOM = require('react-dom')
 
 ReactDOM.render(
-    <h1>Hello world</h1>,
+    <h1>Hello world11111</h1>,
     document.getElementById('example')
 )

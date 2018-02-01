c09f34a82f92eccfc9506367c81324e3fbdbe18b
linpenghui958 <370345882@qq.com>
2018-02-01 22:25:19 +0800

    pc_header

--- src/js/root.js
+++ src/js/root.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router,Route,hashHistory} from 'react-router';
+import {Button} from 'antd';
+import PCIndex from './components/pc_index.js'
+import 'antd/dist/antd.css';
 
 export default class Root extends React.Component{
   render(){
     return (
-    <div>Init</div>
+    <div>
+      <PCIndex></PCIndex>
+    </div>
     );
   };
 }
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015'],
+          presets: ['react', 'env'],
           plugins: ['react-html-attrs'], //添加组件的插件配置
         }
       },
